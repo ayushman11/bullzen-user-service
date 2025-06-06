230ed6c37c431b731d19bf14edc7992ba6e73de9
ayushman11 <ayushman11022002@gmail.com>
2025-06-07 02:04:09 +0530

    handle UserNotFoundException

---
+++ src/main/java/com/bullzen/user/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.bullzen.user.exception;
+
+public class UserNotFoundException extends RuntimeException{
+
+    public UserNotFoundException (String message) {
+        super(message);
+    }
+}
